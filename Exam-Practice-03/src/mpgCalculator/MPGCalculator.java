a050bbf1eb1fd60981208420158a566f93bd05e6
Jojok <Jojok@10.230.40.177>
2022-10-25 12:25:48 -0400

    stuff

--- Exam-Practice-03/src/mpgCalculator/MPGCalculator.java
+++ Exam-Practice-03/src/mpgCalculator/MPGCalculator.java
@@ -1,78 +1,68 @@
 package mpgCalculator;
-	
 
 import javafx.application.Application;
 import javafx.geometry.Pos;
-import javafx.geometry.HPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+//import java.awt.Toolkit;
+//import java.awt.Dimension;
 
-
-public class MPGCalculator extends Application {	
-	private TextField tfAnnualInterestRate = new TextField();
-	private TextField tfNumberOfYears = new TextField();
-	private TextField tfLoanAmount = new TextField();
-	private TextField tfMonthlyPayment = new TextField();
-	private TextField tfTotalPayment = new TextField();
+public class MPGCalculator extends Application {
+	private TextField tfMiles = new TextField();
+	private TextField tfGallons = new TextField();
 	private Button btCalculate = new Button("Calculate");
+	private Label lbOutput = new Label("Output");
 
 	@Override // Override the start method in the Application class
 	public void start(Stage primaryStage) {
 		// Create UI
-		GridPane gridPane = new GridPane();
-		gridPane.setHgap(5);
-		gridPane.setVgap(5);
+		VBox vb1 = new VBox();
+		HBox hb1 = new HBox();
+		HBox hb2 = new HBox();
+		//Dimension d1 = Toolkit.getDefaultToolkit().getScreenSize();
+		//double scaleRatio = 1/5.0;
 		
+		//vb1.setMaxWidth((int)(d1.getWidth()*scaleRatio));
+		lbOutput.setMaxWidth(500);
+		vb1.setStyle("-fx-border-width: 1px; -fx-border-color: black;");
+		hb1.setStyle("-fx-border-width: 1px; -fx-border-color: red;");
+		hb2.setStyle("-fx-border-width: 1px; -fx-border-color: blue;");
+		hb2.setAlignment(Pos.TOP_LEFT);
+		
+		vb1.getChildren().addAll(hb1, hb2);
+		
+		hb2.setMaxWidth(500);
 
-		// Set properties for UI
-		gridPane.setAlignment(Pos.CENTER);
-		tfAnnualInterestRate.setAlignment(Pos.BOTTOM_RIGHT);
-		tfNumberOfYears.setAlignment(Pos.BOTTOM_RIGHT);
-		tfLoanAmount.setAlignment(Pos.BOTTOM_RIGHT);
-		tfMonthlyPayment.setAlignment(Pos.BOTTOM_RIGHT);
-		tfTotalPayment.setAlignment(Pos.BOTTOM_RIGHT);
-		tfMonthlyPayment.setEditable(false);
-		tfTotalPayment.setEditable(false);
-		GridPane.setHalignment(btCalculate, HPos.RIGHT);
-
-		// Process events
-		btCalculate.setOnAction(e -> calculateLoanPayment());
+		hb1.getChildren().add(tfMiles);
+		hb1.getChildren().add(tfGallons);
+		hb2.getChildren().add(lbOutput);
 
 		// Create a scene and place it in the stage
-		Scene scene = new Scene(gridPane, 400, 250);
+		Scene scene = new Scene(vb1, 400, 250);
+		
+		
+		
 		primaryStage.setTitle("MPGCalculator"); // Set title
 		primaryStage.setScene(scene); // Place the scene in the stage
 		primaryStage.show(); // Display the stage
+		hb1.setMaxWidth(primaryStage.getWidth()/2);
+		hb1.maxWidthProperty().bind(primaryStage.maxWidthProperty().multiply(0.5));
 	}
 
 	private void calculateLoanPayment() {
-		// Get values from text fields
-		double interest =
-				Double.parseDouble(tfAnnualInterestRate.getText());
-		int year = Integer.parseInt(tfNumberOfYears.getText());
-		double loanAmount =
-				Double.parseDouble(tfLoanAmount.getText());
 
-		// Create a loan object. Loan defined in Listing 10.2
-		//Loan loan = new Loan(interest, year, loanAmount);
-
-		// Display monthly payment and total payment
-	//	tfMonthlyPayment.setText(String.format("$%.2f",
-	//			loan.getMonthlyPayment()));
-		//tfTotalPayment.setText(String.format("$%.2f",
-		//		loan.getTotalPayment()));
 	}
 
 	/**
-	 * The main method is only needed for the IDE with limited
-	 * JavaFX support. Not needed for running from the command line.
+	 * The main method is only needed for the IDE with limited JavaFX support. Not
+	 * needed for running from the command line.
 	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
 }
-
